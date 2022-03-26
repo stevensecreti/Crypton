8c1e9437b7d2d17e600bae72e5f35fae6b5b3c26
Steven3497 <steven.huynh@stonybrook.edu>
2022-03-26 14:05:59 -0400

    cleaned up Account screen

--- client/src/components/Account/Account.js
+++ client/src/components/Account/Account.js
@@ -1,46 +1,67 @@
-import React, {useState} from 'react';
-import Friend from "../Account/Friend";
-
+import React, { useState }                            from 'react';
+import { Chart as ChartJS, registerables } from "chart.js";
+import {Line} from 'react-chartjs-2';
+import WMHeader from 'wt-frontend/build/components/wmodal/WMHeader';
+import WMMain from 'wt-frontend/build/components/wmodal/WMMain';
+import WLayout from 'wt-frontend/build/components/wlayout/WLayout';
+import WCol from 'wt-frontend/build/components/wgrid/WCol';
+import WRow from 'wt-frontend/build/components/wgrid/WRow';
+import QRCode from '../modals/QRCodeModal';
 
 const Account = (props) => {
+    const balance = props.balance;
+    const buyingPower = props.buyingPower;
+    const trendData = props.balanceData;
+    const accountHex = props.accountHex;
+    //const currentTrend = trendData[trendData.length-1]-trendData[0];
+    //const trend = "Current Trend: " + (currentTrend >= 0 ? "+"+currentTrend+"%" : "-"+(-currentTrend)+"%");
+    //const trendStyle = currentTrend >= 0 ? "account-trend-pos" : "account-trend-neg";
 
-    let friendsList = props.friendsList;
-    console.log("Friends List", friendsList);
-    function addFriend(){
-        props.addFriend();
-    }
+    ChartJS.register(...registerables);
 
-    return(<>
-        <div className="accountScreen">
-            <div className="accountHeader">
-                Account Settings
-            </div>
-            <div className="accountMain">
-                <div className="accountInfo">
-                    Display Name: 
-                    <div className="accountCryptosHeld">
-                        Current Email:
-                        <div className="accountCryptoList">
-                            {//Here map each crypto held into list w bullet points
-                            }
-                            Current Password: <br/> <br/>
-                        </div>
-                        Enable 2FA:
-                    </div>
-                    Enable 2FA:
+    const state = {
+        datasets: [
+          {
+            label: 'Balance',
+            fill: false,
+            lineTension: 0.5,
+            backgroundColor: '#013292',
+            borderColor: '#013292',
+            borderWidth: 2,
+            data: trendData
+          }
+        ]
+      }
+
+      const toggleQRCode = () => {
+        props.setShowQRCode();
+      }
+
+    return(
+        <>
+            <div id="account-container">
+                <div className="account-header">
+                    Account Settings
                 </div>
-                <div className="accountFriends">
-                    Banner
-                    <div className="accountFriendsList">
-                        {//Here map each friend in list to a new friend component <Friend/>
-                            friendsList ? friendsList.map((friend) => <Friend name={friend}/>) : <></>
-                        }
-                    </div>
-                    <button className="accountAddFriend" onClick={addFriend}>Click Here to Change Banner</button>
+                <div className="account-main">
+                        <ul className='account-div'>
+                            <div id="account-balance">
+                                Display Name: test{balance}
+                            </div>
+                            <div id="account-balance">
+                                Current Email: test{buyingPower}
+                            </div>
+                            <div id="account-balance">
+                                Current Password: test{buyingPower}
+                            </div>
+                            
+                            <div id="account-button" onClick={toggleQRCode}>
+                                Banner
+                            </div>
+                        </ul>
                 </div>
             </div>
-        </div>    
-    </>);
-
+        </>
+    );
 }
 export default Account;
